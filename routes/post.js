34c5060a66ae9d897594d6218f9795dfaac49bc6
KIMJINOH97 <rlawlsdh1127@naver.com>
2020-09-08 01:39:32 +0900

    nodebird update

--- routes/post.js
+++ routes/post.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const multer = require('multer');
 const { Post, Hashtag, User } = require('../models');
 const { isLoggendIn, isLoggedIn } = require('./middlewares');
 const router = express.Router();
+
+fs.readdir('uploads', (error) => {
+    if (error) {
+        console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+        fs.mkdirSync('uploads');
+    }
+});
 
 // 이미지 업로드시 폼을 multipart/form-data로, 그리고 이것을 해석하려면 multer가 필요함.
 // 왜냐 express.json 과 express.urlencoded가 해석을 못함 그래서 multer필요
